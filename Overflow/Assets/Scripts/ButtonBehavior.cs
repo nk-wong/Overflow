a1c569c49ce04364d8c5fba2a4b97f4cd9a2df70
Nathan Wong <93850704+nk-wong@users.noreply.github.com>
2022-09-03 15:45:38 -0400

    Removed temporary script

--- Overflow/Assets/Scripts/ButtonBehavior.cs
+++ Overflow/Assets/Scripts/ButtonBehavior.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class ButtonBehavior : MonoBehaviour
 {
 
     public void PlayGame() {
         SceneManager.LoadScene("Game");
     }
 
-    public void ShowRules() {
+    public void LoadRules() {
         SceneManager.LoadScene("Rules");
+    }
+
+    public void LoadMenu() {
+        SceneManager.LoadScene("Menu");
     }
 
     public void QuitGame() {
